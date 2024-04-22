a565a7a46724dcdef560738be5ac427216590c8b
Cha1mae <chaimastaifa@gmail.com>
2024-04-22 08:47:49 -0700

    obl

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,12 @@
 export default function createIteratorObject(report) {
-    let employees = [];
-    for (let department in report.allEmployees) {
-        employees = [...employees, ...report.allEmployees[department]];
+  const emps = [];
+  // eslint-disable-next-line
+  for (const dep of Object.keys(report.allEmployees)) {
+    // eslint-disable-next-line
+    for (const emp of report.allEmployees[dep]) {
+      emps.push(emp);
     }
-    return employeesSymbol.iterator;
+  }
+
+  return emps;
 }
