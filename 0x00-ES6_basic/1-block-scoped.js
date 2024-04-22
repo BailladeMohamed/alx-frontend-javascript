2197f12ae586f4d0d04ae5be62943827ee1f3982
Cha1mae <chaimastaifa@gmail.com>
2024-04-22 06:23:20 -0700

    idk

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,5 +1,7 @@
-// make it a const
-export default function taskBlock(trueOrFalse) {
+// Making task and task2 block-scoped.
+// Changes will be reflected in the function’s output.
+
+export default function taskBlock (trueOrFalse) {
   let task = false;
   let task2 = true;
 
