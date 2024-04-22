886bc91b3cf4e319c80d7fd5a31755b6aff66676
Cha1mae <chaimastaifa@gmail.com>
2024-04-22 06:16:58 -0700

    checking

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+// make it a const
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+
+  return [task, task2];
+}
