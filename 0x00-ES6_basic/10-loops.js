9eedd53d4b7a51be10d035921d6f2168d4323089
Cha1mae <chaimastaifa@gmail.com>
2024-04-22 08:24:52 -0700

    loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let newArray = [];
-  for (let value of array) {
-    newArray.push(appendString + value);
+  // eslint-disable-next-line
+  for (const value of array) {
+    // eslint-disable-next-line
+    array[array.indexOf(value)] = appendString + value;
   }
 
-  return newArray;
+  return array;
 }
