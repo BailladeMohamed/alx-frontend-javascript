50a0370360ac16e08d8f9f6f6b95ccf99c477195
Cha1mae <chaimastaifa@gmail.com>
2024-04-24 08:50:50 -0700

    pwomise

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+// asyncUploadUser calls 2 functions and retrns an obj
+// with the wanted format
+
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+}
