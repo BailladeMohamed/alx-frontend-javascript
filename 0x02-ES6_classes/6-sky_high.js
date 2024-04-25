01416398a4ec95052969b3f6e7375e25eff1595f
Cha1mae <chaimastaifa@gmail.com>
2024-04-25 13:14:31 -0700

    trying

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,17 +1,20 @@
-// nati7at sahab
+// trying this again hope it works
+// nati7at sa7ab
 import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
 
-  get floors() {
-    return (this._floors);
-  }
+  get floors() { return this._floors; }
+
+  set floors(floors) { this._floors = floors; }
 
   evacuationWarningMessage() {
-    return (`Evacuate slowly the ${this.floors} floors`);
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
+
+//export default SkyHighBuilding;//
