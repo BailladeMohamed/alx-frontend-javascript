73c4ff2bbd7895db7de7dfaca16d334bec41f86d
Cha1mae <chaimastaifa@gmail.com>
2024-04-25 14:28:54 -0700

    trying this

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,8 +1,8 @@
 // tonomobila kadir vroom vroom
 import Car from './10-car';
 
-export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
     super(brand, motor, color);
     this._range = range;
   }
@@ -11,3 +11,5 @@ export default class EVCar extends Car {
     return Car;
   }
 }
+
+export default EVCar;
