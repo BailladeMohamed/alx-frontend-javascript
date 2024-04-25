a8fc11fb0265ee3a75dcb8473e3b84d49d81fdf2
Cha1mae <chaimastaifa@gmail.com>
2024-04-25 15:16:27 -0700

    3ad fhmt

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -15,6 +15,4 @@ export default class SkyHighBuilding extends Building {
     return `Evacuate slowly the ${this._floors} floors`;
   }
 }
-//export default SkyHighBuilding;//
-
-
+// export default SkyHighBuilding;//
