849025628fe24bdd484efaae61ba66bf69cc43b9
Cha1mae <chaimastaifa@gmail.com>
2024-04-25 14:24:41 -0700

    3yitt

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,19 +1,13 @@
 // tonomobila kadir vroom vroom
-import Car from './10-car.js';
+import Car from './10-car';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  cloneCar() {
-    const newCar = new Car();
-    newCar._brand = this._brand;
-    newCar._motor = this._motor;
-    newCar._color = this._color;
-    return newCar;
+  static get Symbol.species {
+    return Car;
   }
 }
-
-export default EVCar;
