984d2eef669e74ca32b9a38e2873d22bcad8080f
Cha1mae <chaimastaifa@gmail.com>
2024-04-29 13:59:33 -0700

    tomatoes

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+// Groceries list :)
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
