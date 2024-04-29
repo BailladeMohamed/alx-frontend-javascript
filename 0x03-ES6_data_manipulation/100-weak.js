10994382e96f52d001ab3820870c16e0a80e852e
Cha1mae <chaimastaifa@gmail.com>
2024-04-29 14:25:32 -0700

    idk

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+// i think i get it
+// weakling time
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint);
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, count + 1);
+  }
+}
