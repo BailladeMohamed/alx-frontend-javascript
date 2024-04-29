6891e7a0342dfa05b1e08c0354539722ec7495e4
Cha1mae <chaimastaifa@gmail.com>
2024-04-29 13:27:55 -0700

    city based grades

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+// Specific city students will be
+// graded :)
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students.map((student) => {
+    const foundGrade = newGrades.find((grade) => grade.studentId === student.id);
+    const gradeValue = foundGrade ? foundGrade.grade : 'N/A';
+
+    return {
+      ...student,
+      grade: gradeValue,
+    };
+  }).filter((student) => student.location === city);
+}
+
+export default updateStudentGradeByCity;
